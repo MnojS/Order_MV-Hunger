9126bdd31645a28482898b1cb4528974d13c9edc
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-05-03 18:13:10 +0530

    Add files via upload

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import MenuItems from '../MenuItems/MenuItems'
 import './SideDrawer.css'
+import Aux from '../../hoc/Auxillary/Auxillary'
+import Backdrop from '../../hoc/Backdrop/Backdrop'
 
 
 const SideDrawer = (props) => {
-    
+    let attachedClasses = ['side-drawer','Close'];
+    if(props.open) {
+        attachedClasses = ['side-drawer', 'Open']
+    }
 
     return(
-        <nav className='side-drawer'>
-            <ul>
-                        {MenuItems.map((items , index) => {
-                            return (
-                                <li> 
-                                    <a href={items.url} >
-                                        {items.title}
-                                    </a> 
-                                </li>
-                            )
-                        })}
-                        
-            </ul>
-        </nav>
+        <Aux >
+            <Backdrop show={props.open} clicked={props.closed} />
+            <nav className={attachedClasses.join(' ')}>
+                <ul>
+                            {MenuItems.map((items , index) => {
+                                return (
+                                    <li> 
+                                        <a href={items.url} >
+                                            {items.title}
+                                        </a> 
+                                    </li>
+                                )
+                            })}
+                            
+                </ul>
+            </nav>
+        </Aux>
     )
 
   
