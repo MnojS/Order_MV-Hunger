295aad20dd3bfcd09cd4d953bc46df678989cd34
Manoj Sahu <71431821+MnojS@users.noreply.github.com>
2021-05-03 15:18:09 +0530

    Add files via upload

---
+++ src/components/Home/Button/Buttons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+
+function Buttons() {
+    return (
+        <div>
+             <ButtonGroup className="buttonGroup">
+                <Button 
+                    size="large"
+                    variant="contained"
+                    color="secondary"
+                    >Order Now
+                </Button>
+                <Button 
+                    size="large"
+                    variant="outlined"
+                    color="secondary"
+                    >Order Now
+                </Button>
+            </ButtonGroup>
+        </div>
+    )
+}
+
+export default Buttons;
