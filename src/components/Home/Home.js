58e8c5f534321dc0ed9759de89df4d44585e1fc9
vaibhavirohilla741 <vaibhavirohilla741@gmail.com>
2021-05-04 13:34:24 +0530

    Backdrop working

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import Buttons from '../Home/Button/Buttons'
 import Carousel from 'react-material-ui-carousel'
 import bg_1 from '../../assets/bg_1.png'
 import bg_2 from '../../assets/bg_2.png'
-import ImgMediaCard from './Cart/Cart'
 
 function Home() {
 
@@ -13,7 +12,7 @@ function Home() {
             {/* <Buttons className="buttonGroup"/> */}
             <Carousel autoPlay animation="fade">
                 <div>
-                    <img src={bg_1} className={'homePic'}  />
+                    <img alt="home" src={bg_1} className={'homePic'}  />
                     <div className="Quote">
                         <h3>Welcome 1</h3>
                         <h1>Pizza is not a 'trend' it's a way of life</h1>
@@ -22,7 +21,7 @@ function Home() {
                     </div>
                 </div>
                 <div>
-                    <img src={bg_2} className={'homePic'} />
+                    <img src={bg_2} alt="home"className={'homePic'} />
                     <div className="Quote">
                         <h3>Welcome 2</h3>
                         <h1>Pizza is not a 'trend' it's a way of life</h1>
--- src/components/hoc/Backdrop/Backdrop.js
+++ src/components/hoc/Backdrop/Backdrop.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Backdrop.css'
 
 const Backdrop = (props) => (
     props.show ? <div 
