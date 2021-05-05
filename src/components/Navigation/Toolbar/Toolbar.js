6dd0e0e17ff2d10cb0faeb83464bc614dcca94d8
Manoj Sahu <71431821+MnojS@users.noreply.github.com>
2021-05-05 20:03:49 +0530

    Update Toolbar.js

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,24 +3,26 @@ import MenuItems from "../MenuItems/MenuItems"
 import "../Toolbar/Navbar.css"
 import PizzaLogo from '../../../assets/PizzaLogo.png'
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
-import "../SideDrawer/DrawerToggleButton.css"
+// import "../SideDrawer/DrawerToggleButton.css"
+import { useId } from "react-id-generator";
 
 const Toolbar = (props) => (
+    
     <header className= "Toolbar">
+        
         <div>
              <DrawerToggleButton click={props.drawerClickHandler}/>
             </div>
         <nav className="DesktopOnly">
         
            <div className="NavbarItems">
-            
-            <h1 className='navbar-logo'>
-                <img alt="logo" src={PizzaLogo} className="Logo" />
+            <h1 className='navbar-logo'>        
+                {/* <img alt="logo" src={PizzaLogo} className="Logo" /> */}
             </h1>
             <ul className = 'nav-menu' >
-                    {MenuItems.map((items , index) => {
+                    {MenuItems.map((items ) => {
                         return (
-                            <li> 
+                            <li key={items.title}> 
                                 <a className={items.cName} href={items.url} >
                                     {items.title}
                                 </a> 
