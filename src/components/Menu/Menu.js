4af5e9b955cb956514e55589fa1210187ddf22b1
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-05-13 17:08:19 +0530

    Add files via upload

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Menu.css'
+
+class Menu extends Component {
+    componentDidMount() {
+        console.log(this.props);
+
+    }
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+export default Menu
+
