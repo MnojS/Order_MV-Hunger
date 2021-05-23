555b3e3666e5e0ddd6e4ff2f2283a8d838062f9e
Manoj Sahu <mnojsha413@gmail.com>
2021-05-23 12:46:25 +0530

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import Box from '@material-ui/core/Box'
 
 function Footer() {
 	return (
-		<footer style={{fontFamily:"Pangolin, cursive"}}>
+		<footer >
 			<Box 
 				bgcolor='rgba(17, 15, 15, 0.993)' 
 				color='white' 
---
+++ src/components/OurMenu/MenuAbout/MenuAbout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./MenuAbout.css"
+function MenuAbout() {
+    return (
+        <div className="MenuAbout">
+            <div className="text">
+                <span>Our Menu</span>
+                <hr />
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and <br />Consonantia, there live the blind texts.</p>
+            </div>
+           
+        </div>
+    )
+}
+
+export default MenuAbout
