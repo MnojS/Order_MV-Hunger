4bc6d0af5cdc754959c6c0a54d0e9bb11c1369f2
Manoj Sahu <mnojsha413@gmail.com>
2021-05-24 20:32:31 +0530

    quantity

--- src/components/OurMenu/MenuItems/MenuItems.js
+++ src/components/OurMenu/MenuItems/MenuItems.js
@@ -1,33 +1,9 @@
-import React, {useState} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import { Button } from '@material-ui/core';
+import React from 'react'
 import './MenuItems.css'
-
-
-const useStyles = makeStyles(() => ({
-    root: {
-      maxWidth: 345,
-    },
-
-    media: {
-      height: 5,
-      paddingTop: '56.25%', // 16:9
-    },
+import ItemCart from './ItemCart';
   
-  }));
-  
-
 function MenuItems() {
 
-    const classes = useStyles();
-    const [count , setCount] = useState(0);
 
     const menus = [
         {
@@ -151,42 +127,9 @@ function MenuItems() {
 
     return (
         <div className="MenuItems">
-            {
-               menus.map(
-                   (items) => {
-                       return (
-                           <div key={items.id} className="cardMenu" style={{maxWidth:"300px" }}>
-                                <Card className={"root"} style={{color:"white" , backgroundColor:"black" , cursor:"pointer" ,  }} >
-                                    <CardHeader 
-                                        title={items.title}
-                                    />
-                                    <CardMedia
-                                        className={classes.media}
-                                        image={items.src}
-                                    />
-                                    <CardContent>
-                                        <Typography variant="body2" color="" component="p">
-                                        A perfect party dish and a fun meal to cook together with your
-                                        guests. <br /> <hr />
-                                        <span><h4 >Price : {items.price} $</h4></span>
-                                        <span>Quantity : 
-                                            <Button onClick={ () => setCount(count - 1) }  variant="outlined" color="default" style={{color:"white" , }}>-</Button> 
-                                            {count} 
-                                            <Button onClick={ () => setCount(count + 1) } variant="outlined" color="default" style={{color:"white" , }}>+</Button> </span> 
-                                        </Typography>
-                                    </CardContent>
-                                        <Button  variant="text" color="secondary" style={{marginLeft:"100px" , color:"orange"}}>
-                                            <IconButton style={{backgroundColor:"transparent" , color:"orange" , opacity:"" }}>
-                                            <AddShoppingCartIcon color="" />
-                                            </IconButton>
-                                                Add to Cart
-                                        </Button>
-                                </Card>
-                           </div>
-                       )
-                   }
-               )
-           }
+           {menus.map((items) => {
+               return <ItemCart items={items} />
+           })}
         </div>
     )
 }
