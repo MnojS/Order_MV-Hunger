8cffdc52e447f641892c4f430162dc58107063e6
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-05-26 10:48:03 +0530

    Changes in services js

--- src/containers/Services/Services.js
+++ src/containers/Services/Services.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import OurServices from '../../components/OurServices/OurServices'
 import './Services.css'
 
 function Services() {
     return (
         <div className="Services">
-            Services
+            <OurServices/>
         </div>
     )
 }
